726462050d7b48ce7766db9e5c8dcc84f948147d
betsa96hye <105254730+betsa96hye@users.noreply.github.com>
2022-05-09 20:19:11 -0500

    Add files via upload

---
+++ productos.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Productos</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css'>
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Capriola'>
+
+<h2 style=" position:absolute; top:10px; left:30px; color:black;">Productos</h2>
+</head>
+<body>
+        
+<br><p style=" position:absolute; top:70px; left:30px; color:black;">Cliente: <?php echo $user->getNombre();  ?> </div>
+
+<div class="amazing-menu">
+  <div class="menu-item">
+    <div class="container">
+      <div class="title"><a href="#" style ="position:absolute; top:90px; left:900px; color:blue;">Cerrar Sesion</a></div>
+      <div class="arrow"><span class="ion-chevron-right"></span></div>
+      <div class="rating -r2" href="#">
+
+      </div>
+    </div>
+  </div>
+  <div class="menu-item">
+    <div class="container">
+      <div class="title"><a href="carrito.php " style ="position:absolute; top:90px; left:1100px; color:blue;">Ir al carrito de compras</a></div>
+      <div class="arrow"><span class="ion-chevron-right"></span></div>
+      <div class="rating -r5" href="carrito.php">
+        
+      </div>
+    </div>
+  </div>
+    
+</body>
+</html>
