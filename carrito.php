9317712f2bed234bb89862339b416db7e3bac769
betsa96hye <105254730+betsa96hye@users.noreply.github.com>
2022-05-09 20:17:36 -0500

    Add files via upload

---
+++ carrito.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Carrito de compras</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css'>
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Capriola'>
+
+<h2 style=" position:absolute; top:10px; left:30px; color:black;">Carrito de compras</h2>
+</head>
+<body>
+        
+<br><p style=" position:absolute; top:70px; left:30px; color:black;">Nombre Cliente: </div>
+<br><p style=" position:absolute; top:120px; left:30px; color:black;">Fecha: </div>
+<br><p style=" position:absolute; top:120px; left:400px; color:black;">Numero de referencia: </div>
+
+
+<div class="amazing-menu">
+  <div class="menu-item">
+    <div class="container">
+      <div class="title"><a href="#" style ="position:absolute; top:90px; left:1100px; color:blue;">Cerrar Sesion</a></div>
+      <div class="arrow"><span class="ion-chevron-right"></span></div>
+      <div class="rating -r2" href="#">
+      </div>
+   </div>
+</div>
+
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+include_once 'includes_BD/user.php';
+include_once 'includes_BD/user_session.php';
+$userSession = new UserSession();
+$user = new User();
+if(isset($_SESSION['user'])){
+    //echo "hay sesion";
+    $user->setUser($userSession->getCurrentUser());
+    include_once 'vistas/productos.php';
+   
+}else if(isset($_POST['username']) && isset($_POST['password'])){
+    
+    $userForm = $_POST['username'];
+    $passForm = $_POST['password'];
+    $user = new User();
+    if($user->userExists($userForm, $passForm)){
+        //echo "Existe el usuario";
+        $userSession->setCurrentUser($userForm);
+        $user->setUser($userForm);
+        include_once 'vistas/productos.php';
+    }else{
+        //echo "No existe el usuario";
+        $errorLogin = "Nombre de usuario y/o password incorrecto";
+        include_once 'vistas/iniciar_sesion.php';
+    }
+}else{
+    //echo "login";
+    include_once 'vistas/iniciar_sesion.php'; //donde se encuentra el formulario de usuario y contraseña
+}
+?>
